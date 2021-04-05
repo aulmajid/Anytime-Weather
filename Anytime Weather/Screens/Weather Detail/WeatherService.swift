95eca1b96b2a0efa08c1bde75f20fa5eae826be9
ilaumjd <ilaumjd@gmail.com>
2021-04-06 05:40:04 +0700

    git ignore api token

--- Anytime Weather/Screens/Weather Detail/WeatherService.swift
+++ Anytime Weather/Screens/Weather Detail/WeatherService.swift
@@ -10,7 +10,7 @@ import Alamofire
 class WeatherService {
     
     func getWeather(city: String, completion: @escaping ((Weather?) -> ())) {
-        AF.request("https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=7bbd9a7ad38be2e7af7f8d5d41689c8d").responseDecodable(of: Weather.self) { response in
+        AF.request("https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(TokenConstants.openweathermap)").responseDecodable(of: Weather.self) { response in
             debugPrint(response)
             switch response.result {
             case .success(let weather):
---
+++ Anytime Weather/TokenConstants.swift
@@ -0,0 +1,14 @@
+//
+//  TokenConstants.swift
+//  Anytime Weather
+//
+//  Created by aku pintar indonesia on 06/04/21.
+//
+
+import Foundation
+
+struct TokenConstants {
+    
+    static let openweathermap = "7bbd9a7ad38be2e7af7f8d5d41689c8d"
+    
+}
