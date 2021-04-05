37238fa12441331260513a660d69497832acd223
ilaumjd <ilaumjd@gmail.com>
2021-04-05 13:34:14 +0700

    Initial Commit

---
+++ Anytime Weather/Anytime_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anytime_WeatherApp.swift
+//  Anytime Weather
+//
+//  Created by aku pintar indonesia on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Anytime_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anytime Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anytime Weather
+//
+//  Created by aku pintar indonesia on 05/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
