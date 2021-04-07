711f2902180321fd8142236b27bdcc3378d28182
ilaumjd <ilaumjd@gmail.com>
2021-04-07 09:00:32 +0700

    test - search test refactor

--- Anytime Weather Tests/Anytime_Weather_Tests.swift
+++ Anytime Weather Tests/Anytime_Weather_Tests.swift
@@ -8,20 +8,24 @@
 import XCTest
 
 class Anytime_Weather_Tests: XCTestCase {
-
-    func test_SearchRecommendsHistory_InputEmpty() {
-        let vm = SearchViewModel()
-        let keywords = vm.getRecommendations(keyword: "")
-        for keyword in keywords {
-            XCTAssertEqual(keyword.isHistory, true)
-        }
-    }
     
-    func test_SearchRecommendsNewKeywords_InputNotEmpty() {
-        let vm = SearchViewModel()
-        let keywords = vm.getRecommendations(keyword: "Solo")
-        for keyword in keywords {
-            XCTAssertEqual(keyword.isHistory, false)
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
         }
     }
 
