04b4038cd5946a240806824995e260a593e646c5
ilaumjd <ilaumjd@gmail.com>
2021-04-07 12:45:29 +0700

    weather city exists

---
+++ Anytime Weather Tests/WeatherTests.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherTests.swift
+//  Anytime Weather Tests
+//
+//  Created by aku pintar indonesia on 07/04/21.
+//
+
+import XCTest
+
+class WeatherTests: XCTestCase {
+
+    private var vm: WeatherDetailViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        self.vm = WeatherDetailViewModel(weatherService: MockWeatherService())
+    }
+    
+    func test_TemperatureSuffix() {
+        let settings = AppSettings()
+        self.vm.settings = settings
+        
+        settings.unit = Unit.celcius
+        XCTAssertEqual(self.vm.suffix, "°C")
+        
+        settings.unit = Unit.fahrenheit
+        XCTAssertEqual(self.vm.suffix, "°F")
+    }
+    
+    func test_cityAvailable() {
+        self.vm.city = "Surabaya"
+        self.vm.fetchWeather()
+        XCTAssertNotNil(vm.weather.main)
+    }
+    
+    func test_cityNotFound() {
+        self.vm.city = "Lautan"
+        self.vm.fetchWeather()
+        XCTAssertNil(vm.weather.main)
+    }
+
+}
