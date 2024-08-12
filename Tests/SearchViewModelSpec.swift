bf637b9cfb64170f647ae8828e1a051259a9027f
ilaumjd <ilaumjd@gmail.com>
2024-08-12 21:13:06 +0700

    generate mocks using mockolo

--- Tests/SearchViewModelSpec.swift
+++ Tests/SearchViewModelSpec.swift
@@ -14,14 +14,23 @@ class SearchViewModelSpec: QuickSpec {
         var viewModel: SearchViewModel!
         
         beforeEach {
-            viewModel = SearchViewModel()
+            // Reset handlers before each test
+            KeywordProviderMock.historyCallCount = 0
+            KeywordProviderMock.predictCallCount = 0
+            viewModel = SearchViewModel(keywordProvider: KeywordProviderMock.self)
         }
         
         describe("getRecommendations") {
             context("when the keyword is empty") {
                 it("should return history keywords") {
+                    let historyKeywords = [Keyword(name: "History1", isHistory: true), Keyword(name: "History2", isHistory: true)]
+                    KeywordProviderMock.historyHandler = { historyKeywords }
+                    
                     let recommendations = viewModel.getRecommendations(keyword: "")
-                    expect(recommendations).to(equal(Keyword.history()))
+                    
+                    expect(recommendations).to(equal(historyKeywords))
+                    expect(KeywordProviderMock.historyCallCount).to(equal(1))
+                    expect(KeywordProviderMock.predictCallCount).to(equal(0))
                 }
             }
             
@@ -30,11 +39,16 @@ class SearchViewModelSpec: QuickSpec {
                 var recommendations: [Keyword]!
                 
                 beforeEach {
+                    let predictedKeywords = [Keyword(name: "York", isHistory: false), Keyword(name: "Jersey", isHistory: false)]
+                    KeywordProviderMock.predictHandler = { predictedKeywords }
+                    
                     recommendations = viewModel.getRecommendations(keyword: keyword)
                 }
                 
                 it("should return non-empty recommendations") {
                     expect(recommendations).notTo(beEmpty())
+                    expect(KeywordProviderMock.predictCallCount).to(equal(1))
+                    expect(KeywordProviderMock.historyCallCount).to(equal(0))
                 }
                 
                 it("should have recommendations starting with the keyword") {
